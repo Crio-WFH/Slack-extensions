557392e1e599592f32523b1ef2fdb5772fd73a45
Nimisha Yadav <iamnimishayadav@gmail.com>
2021-07-12 16:46:13 +0530

    Update config.js

--- Slackmarks/config.js
+++ Slackmarks/config.js
@@ -1,3 +1,3 @@
 const token = "<API Token>";    //starting with `xoxb`
-const channel = "@iamnimishayadav"; 
+const channel = "@iamnimishayadav"; //insert your username
 const slackUrl = "https://slack.com/api/chat.postMessage";    // Magic Url
